75ffa6543f9d5aaa134170d098a53a1cab56080a
eabernardino <eliezer.abernardino@mercadolivre.com>
2023-02-09 11:41:38 -0300

    corrigindo camadas

--- src/main/java/com/generation/blogpessoal/model/UsuarioLogin.java
+++ src/main/java/com/generation/blogpessoal/model/UsuarioLogin.java
@@ -1,7 +1,19 @@
 package com.generation.blogpessoal.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
 public class UsuarioLogin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    //private Long id;
     private String nome;
     private String usuario;
     private String senha;
